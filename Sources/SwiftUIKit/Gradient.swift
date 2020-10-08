a230a03de14c3cee597fc3399fb3d9b23b3409e8
chenhaoyu.1999 <chenhaoyu.1999@bytedance.com>
2020-10-08 17:30:40 +0800

    访问权限修改

--- Sources/SwiftUIKit/Gradient.swift
+++ Sources/SwiftUIKit/Gradient.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 extension CGPoint {
-    static let top = CGPoint(x: 0.5, y: 0)
-    static let bottom = CGPoint(x: 0.5, y: 1)
-    static let leading = CGPoint(x: 0, y: 0.5)
-    static let trailing = CGPoint(x: 1, y: 0.5)
+    public static let top = CGPoint(x: 0.5, y: 0)
+    public static let bottom = CGPoint(x: 0.5, y: 1)
+    public static let leading = CGPoint(x: 0, y: 0.5)
+    public static let trailing = CGPoint(x: 1, y: 0.5)
 }
 
 
